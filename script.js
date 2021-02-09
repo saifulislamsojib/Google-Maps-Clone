a0c382c05df46a05adaa628b98c5495ad65befa8
saifulislamsojib <saifulislam.sojib56@gmail.com>
2021-02-09 14:48:52 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FpZnVsaXNsYW0tc29qaWIiLCJhIjoiY2treHF5Y2ZkMDhmdjJubXpndHN5M21nayJ9.rW0bkIs1IHeZUHom8rz8bw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
